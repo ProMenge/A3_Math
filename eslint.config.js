111bedbb95ea75d8f3c3afc132d3152b15562442
ProMenge <mengeprofissional@gmail.com>
2025-05-18 19:30:32 -0300

    :package: build: adding editorconfig and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,33 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import configPrettier from "eslint-config-prettier";
+import prettier from "eslint-plugin-prettier";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      semi: "error",
+      "prefer-const": "error",
+      // Prettier como regra de erro
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
     },
   },
-)
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    // Configuração do Prettier para desativar regras conflitantes
+    plugins: { prettier },
+    rules: configPrettier.rules,
+  },
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
+];
